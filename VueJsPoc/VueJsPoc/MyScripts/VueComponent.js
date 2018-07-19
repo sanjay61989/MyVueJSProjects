9df42e785ff2e46cea81e7657e871dd83754827f
Sanjay Chhabria <sonu61989@gmail.com>
2018-07-19 20:16:13 +0530

    created vue js poc basics and its life cycle (#1)

---
+++ VueJsPoc/VueJsPoc/MyScripts/VueComponent.js
@@ -0,0 +1,52 @@
+﻿Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        groceryList: [
+          { id: 0, text: 'Vegetables' },
+          { id: 1, text: 'Cheese' },
+          { id: 2, text: 'Whatever else humans are supposed to eat' }
+        ]
+    }
+})
+
+var app8 = new Vue({
+    el: "#app-8",
+    data: {
+        a: 'hello'        
+    },
+    beforeCreate: function () {
+        console.log('before create');
+        this.b = this.b + ' before created';
+    },
+    created: function () {
+        console.log('created');
+    },
+    beforeMount: function () {
+        console.log('before mount');
+    },
+    mounted: function () {
+        console.log('mounted');
+    },
+    beforeUpdate: function () {
+        console.log('before update');
+    },
+    updated: function () {
+        console.log('updated');
+    },
+    beforeDestroy: function () {
+        console.log("before destroy")
+    },
+    destroyed: function () {
+        console.log("destroyed")
+    },
+    methods: {
+        destroy: function () {
+            this.$destroy();
+        }
+    }
+})
---
+++ VueJsPoc/VueJsPoc/MyScripts/index.js
@@ -0,0 +1,43 @@
+﻿var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    },
+    methods: {
+        reverseMessage: function () {
+            this.seen = !this.seen;
+        }
+    }
+})
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+          { text: 'Learn JavaScript' },
+          { text: 'Learn Vue' },
+          { text: 'Build something awesome' }
+        ]
+    }
+})
+
+var app6 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
